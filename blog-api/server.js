85da33bee96ccdc71f05c9d0b1b4c97fb2198b94
siddharathcodes <siddharathcodes@gmail.com>
2026-03-24 20:46:52 +0545

    fixing proxy error of exress rete limit

--- blog-api/server.js
+++ blog-api/server.js
@@ -10,8 +10,12 @@ dotenv.config()
 connectDB()
 
 const app = express()
+app.set('trust proxy', 1)
 app.use(helmet())
-app.use(cors({ origin: process.env.CLIENT_URL }))
+app.use(cors({ 
+  origin: process.env.CLIENT_URL,
+  credentials: true
+ }))
 app.use(express.json())
 
 app.use('/api/auth', authRoutes)
