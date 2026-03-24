12d0d02ff93b2cd6611841a098809f642bc29ae3
siddharathcodes <siddharathcodes@gmail.com>
2026-03-24 21:08:52 +0545

    fixing email ip

--- blog-api/utils/sendEmail.js
+++ blog-api/utils/sendEmail.js
@@ -5,15 +5,18 @@ const sendEmail = async (to,subject,text)=>{
    try{
      // connection to the gmail
     const transporter = nodemailer.createTransport({
-        service : 'gmail',
-        auth :{
-            user :process.env.EMAIL_USER,
-            pass :process.env.EMAIL_PASS,
-        }, 
-        tls: {
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    },
+    tls: {
         rejectUnauthorized: false
-    }
-    })
+    },
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000
+})
 
     // sender email to other
     const mailOption ={
