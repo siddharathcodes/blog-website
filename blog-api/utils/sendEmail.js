a25273d56af782a8c7640dfd4fe8d0e986bfbd47
siddharathcodes <siddharathcodes@gmail.com>
2026-03-24 21:28:24 +0545

    changing email to brevo

--- blog-api/utils/sendEmail.js
+++ blog-api/utils/sendEmail.js
@@ -1,38 +1,31 @@
 const nodemailer = require('nodemailer')
 
-const sendEmail = async (to,subject,text)=>{
-
-   try{
-     // connection to the gmail
+const sendEmail = async (to, subject, text) => {
+  try {
     const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
-    tls: {
-        rejectUnauthorized: false
-    },
-    connectionTimeout: 10000,
-    greetingTimeout: 10000,
-    socketTimeout: 10000
-})
+      host: "smtp-relay.brevo.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.EMAIL_USER, //brevo name
+        pass: process.env.EMAIL_PASS, // your Brevo SMTP key
+      },
+    })
 
-    // sender email to other
-    const mailOption ={
-        from :process.env.EMAIL_USER,
-        to,
-        subject,
-        text
+    const mailOption = {
+      from: process.env.EMAIL_USER, // or verified sender email
+      to,
+      subject,
+      text,
     }
-    await transporter.sendMail(mailOption)
-        console.log("Email sent to ", to)
-   }
 
-   catch(error){
-    console.error("error sent ", error.message)
+    await transporter.sendMail(mailOption)
+    console.log("Email sent to", to)
+
+  } catch (error) {
+    console.error("Error sending:", error.message)
     throw new Error('Email could not be sent')
-   }
+  }
 }
 
 module.exports = sendEmail
